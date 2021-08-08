06ba7f63094f665d45fbefc52f3c07955f7eca95
Florian Maier <florian.maier2@liebherr.com>
2021-08-08 20:55:39 +0200

    refactoring of ast / identifier into separate file

--- ast/ast.go
+++ ast/ast.go
@@ -2,7 +2,6 @@ package ast
 
 import (
 	"bytes"
-	"monkey/token"
 )
 
 type Node interface {
@@ -35,19 +34,4 @@ func (p *Program) String() string {
 	}
 
 	return out.String()
-}
-
-type Identifier struct {
-	Token token.Token
-	Value string
-}
-
-func (i *Identifier) expressionNode() {}
-
-func (i *Identifier) TokenLiteral() string {
-	return i.Token.Literal
-}
-
-func (i *Identifier) String() string {
-	return i.Value
 }
