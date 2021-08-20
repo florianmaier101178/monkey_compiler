a39f8b5328a264f98d6b64e988ece197a00d4ced
Florian Maier <florian.maier2@liebherr.com>
2021-08-20 10:22:24 +0200

    first version of object / value system

--- repl/repl.go
+++ repl/repl.go
@@ -53,6 +53,6 @@ func printParseErrors(out io.Writer, errors []string) {
 	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
 	io.WriteString(out, "parser errors:\n")
 	for _, msg := range errors {
-		io.WriteString(out, "\t" + msg + "\n")
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
