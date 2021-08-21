8c21b4d9f90bd2d491161298b54db13f055313c6
Florian Maier <florian.maier2@liebherr.com>
2021-08-21 20:14:06 +0200

    evaluation of functions and function calls

---
+++ object/function.go
@@ -0,0 +1,39 @@
+package object
+
+import (
+	"bytes"
+	"monkey/ast"
+	"strings"
+)
+
+const (
+	FunctionObj = "FUNCTION"
+)
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType {
+	return FunctionObj
+}
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
