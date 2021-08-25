2f67f52e47ae6fc330b58a86c5baa5b22c3bfeba
Florian Maier <florian.maier2@liebherr.com>
2021-08-25 16:53:43 +0200

    evaluate array index operator expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -113,6 +113,17 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return elements[0]
 		}
 		return &object.Array{Elements: elements}
+
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	}
 
 	return nil
@@ -330,4 +341,25 @@ func evalStringInfixExpression(operator string, left, right object.Object) objec
 	leftValue := left.(*object.String).Value
 	rightValue := right.(*object.String).Value
 	return &object.String{Value: leftValue + rightValue}
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ArrayObj && index.Type() == object.IntegerObj:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
 }
