8cfcff73b2bb7cd46c2e3e8ed9fa9ede037d6b12
Florian Maier <florian.maier2@liebherr.com>
2021-08-26 14:30:03 +0200

    first / smallest compilation

---
+++ compiler/bytecode.go
@@ -0,0 +1,18 @@
+package compiler
+
+import (
+	"monkey/code"
+	"monkey/object"
+)
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
